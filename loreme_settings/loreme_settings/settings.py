768240d9e5a5c3ef1b207d61c8102f97d2d77ac2
ubuntu-miku <plazasjolifenzio@gmail.com>
2025-11-11 03:27:17 -0300

    nada

--- loreme_settings/loreme_settings/settings.py
+++ loreme_settings/loreme_settings/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
